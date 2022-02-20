32b15aa09050cff0162940a0622c59064867462e
Rohit Kumar <sonu12345kumar12345@gmail.com>
2022-02-20 13:30:10 +0530

    add: DB

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var _ = require('lodash');
+const mongoose = require('mongoose');
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -11,68 +10,67 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-let posts = [];
 
 app.set('view engine', 'ejs');
+
+mongoose.connect("mongodb://localhost:27017/postDB", { useNewUrlParser: true })
+
+const postSchema = {
+  title: String,
+  content: String
+}
+const Post = mongoose.model("Post", postSchema)
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-
-  res.render("home", { homeHomeStartingContent: homeStartingContent, posts: posts });
-
-
+  Post.find({}, function (err, posts) {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+    });
+  })
 });
+
 app.get("/about", function (req, res) {
-
-  res.render("about", { homeAboutContent: aboutContent });
-
+  res.render("about", {
+    homeAboutContent: aboutContent
+  });
 });
+
 app.get("/contact", function (req, res) {
-
-  res.render("contact", { homeAboutContent: aboutContent });
-
+  res.render("contact",
+    { homeAboutContent: aboutContent });
 });
 
 app.get("/compose", function (req, res) {
-
   res.render("compose");
-
 });
 
 app.post("/compose", function (req, res) {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
-  posts.push(post);
-  // console.log(posts.title);
-  res.redirect("/");
-
+  const post = new Post({
+    title: _.upperCase(req.body.postTitle),
+    content: _.upperFirst(req.body.postBody)
+  })
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
 
-app.get('/posts/:postName', function (req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(function (post) {
-    const storedTitle = _.lowerCase(post.title);
-    if (storedTitle === requestedTitle) {
-      console.log("match found");
-
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-
-
-    }
-
-
-  })
-})
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    res.render("post", {
+      title: _.upperCase(post.title),
+      content: _.upperFirst(post.content)
+    });
+  });
+});
 
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started on the port 3000");
+  console.log("Server running");
 });
